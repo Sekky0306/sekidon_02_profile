a0f1e268e0ecd88f988b6196abfb0d40208b1563
YukiSekido <step1336@gmail.com>
2021-05-12 12:41:21 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 関戸優紀 on 2021/05/12.
+//  Copyright © 2021 app.sekido.sekidon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
